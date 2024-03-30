c50b06bdb669b5ffe6d0cdf095b4d248f50891cc
AryanRanderiyaWorkstation <aryan.k.randeriya@gmail.com>
2024-03-30 17:02:35 +0530

    Delete laptop venv, added documentation to code

--- main.py
+++ main.py
@@ -5,27 +5,29 @@ from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 import os.path
 import threading
-import time
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/contacts.readonly",
           "https://www.googleapis.com/auth/directory.readonly"]
 
-data_list = []
+data: list = []
 
 
-def main():
-    """Shows basic usage of the People API.
-    Prints the name of the first 10 connections.
+def authenticate() -> None:
     """
+    Authenticate user using Google Auth 2.0
+    """
+
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
+        print("User not authenticated... Authentication flow Started...")
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
@@ -33,27 +35,38 @@ def main():
                 "credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=8080)
+
         # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
     try:
         service = build("people", "v1", credentials=creds)
-        fetch_contacts(service)
+        print("User Authenticated!")
+        fetch_directory_contacts(service)
 
     except HttpError as err:
         print(err)
 
 
-def fetch_contacts(service):
+def fetch_contacts():
+    ...
 
-    finished = False
-    next_page_token = None
-    data = set()
-    count = 0
+
+def fetch_directory_contacts(service) -> None:
+    """
+    Fetch all the contacts
+    """
+    finished: bool = False
+    next_page_token: str = None
+    data: set = set()
+    count: int = 0
+
+    print("Fetching Contacts...")
 
     while not finished:
 
+        # Fetch results of listing directory
         results = service.people().listDirectoryPeople(
             readMask='names,emailAddresses',
             pageSize=1000,
@@ -63,52 +76,66 @@ def fetch_contacts(service):
 
         result = results.get('people', [])
 
-        count += len(result)
+        count += len(result)  # Increment the count of records fetched
         print(f"Fetching Contacts {count}...", end='\r')
 
-        if not result:
-            finished = True
+        # If no more results then exit the while loop
+        finished = not result
 
         for person in result:
-            emailAddresses = person.get("emailAddresses", [])
+            # Store email addresses (if multiple) in a list after fetching from dict
+            emailAddresses: list = person.get("emailAddresses", [])
 
+            # Check if the value exists
             if emailAddresses:
-                for email in emailAddresses:
+                for email in emailAddresses:  # Iterate over lists
                     data.add(email['value'].strip())
 
+        # Fetch the next page of results (1k)
         next_page_token = results.get('nextPageToken')
 
-        finished = (next_page_token is None)
+        # If no more results then exit the while loop
+        finished = not next_page_token
 
     write_to_file(data)
 
 
-def perform_write(list, filename):
+def perform_write(list: list, filename: str) -> None:
+    """
+    Write the contents of a list to the specified filename
+    """
     with open(filename, "w") as file:
         for i, data in enumerate(list):
             print(f"Progress (Writing to File): {i}/{len(list)}", end='\r')
             file.write(data + "\n")
 
 
-def write_to_file(data_set):
-
-    data_list = sorted(data_set)
-    data_list_sorted_domain = sorted(data_set, key=sort_by_domain)
+def write_to_file(data_set: set) -> None:
+    """
+    Create 2 threads to write to 2 files.
+    File 1 (directory_emails_alphabetic.txt) contains emails sorted by their name.
+    File 2 (directory_emails_domain.txt) contains emails sorted by the domain
+    """
+    data: list = sorted(data_set)
+    data_sortby_domain: list = sorted(data_set, key=domain)
 
     thread1 = threading.Thread(target=perform_write, args=(
-        data_list, "emails_alphabetic.txt"))
+        data, "directory_emails_alphabetic.txt"))
 
     thread2 = threading.Thread(target=perform_write, args=(
-        data_list_sorted_domain, "emails_domain.txt"))
+        data_sortby_domain, "directory_emails_domain.txt"))
 
     thread1.start()
     thread2.start()
 
 
-def sort_by_domain(email):
+def domain(email) -> str:
+    """
+    Return the domains of the specified email
+    """
     _, domain = email.split('@')
     return domain
 
 
 if __name__ == "__main__":
-    main()
+    authenticate()
